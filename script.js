b66ba888fddffad9048235843a66c88c3731b158
kelseyforever <collinczack@gmail.com>
2025-08-20 03:20:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ function load(type, list) {
       const vid = document.createElement('video');
       vid.src = `assets/videos/${name}#t=0.1`;
       vid.muted = true;
+      vid.poster = `assets/videos/${name}.jpg`;
       vid.onmouseenter = () => vid.play();
       vid.onmouseleave = () => vid.pause();
       vid.onclick       = () => openLightbox(vid.src, 'video');
